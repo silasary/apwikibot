5fd7e1403b4f6e9010f4f68b76de025e882866c5
Katelyn Gigante <clockwork.singularity@gmail.com>
2025-10-24 16:03:20 +1100

    Initial commit

---
+++ GamePageChecks.cs
@@ -0,0 +1,71 @@
+﻿using MwParserFromScratch;
+using WikiClientLibrary.Pages;
+using System.Linq;
+using MwParserFromScratch.Nodes;
+
+internal static class GamePageChecks
+{
+
+    public async static Task<bool> CheckTemplates(WikiPage member)
+    {
+        Console.WriteLine($"Checking {member.Title}");
+        await member.RefreshAsync(PageQueryOptions.FetchContent | PageQueryOptions.ResolveRedirects);
+
+        var parser = new WikitextParser();
+        var ast = parser.Parse(member.Content);
+
+        var allNodes = ast.EnumDescendants().ToList();
+
+        var headerParagraph = allNodes.OfType<Paragraph>().FirstOrDefault();
+
+        var infobox = allNodes.OfType<Template>().Where(n => n.Name.ToPlainText() == "Infobox game").FirstOrDefault();
+        var asboxes = allNodes.OfType<Template>().Where(n => n.Name.ToPlainText() == "asbox").ToList();
+        var gamestub = allNodes.OfType<Template>().Where(n => n.Name.ToPlainText() == "Game stub").FirstOrDefault();
+        var notracker = allNodes.OfType<Template>().Where(n => n.Name.ToPlainText() == "NoTracker").FirstOrDefault();
+
+        if (infobox == null)
+        {
+            Console.WriteLine($"{member.Title} is missing an Infobox!");
+        }
+        var infoboxIndex = allNodes.IndexOf(infobox);
+        var notrackerIndex = allNodes.IndexOf(notracker);
+        if (notracker != null && infoboxIndex < notrackerIndex)
+        {
+            Console.WriteLine("NoTracker was after Infobox");
+            notracker.Remove();
+            headerParagraph.Prepend(notracker.ToString() + "\n");
+        }
+        gamestub ??= asboxes.FirstOrDefault();
+        var gamestubIndex = allNodes.IndexOf(gamestub);
+        if (gamestub != null && infoboxIndex > gamestubIndex)
+        {
+            Console.WriteLine("Infobox was after Game Stub");
+            gamestub.Remove();
+            headerParagraph.Append(gamestub.ToString() + "\n");
+        }
+        
+        var newContent = ast.ToString();
+        string hparatext = headerParagraph.ToString();
+        string oparatext = hparatext;
+        while (hparatext.Contains("\n\n"))
+        {
+            hparatext = hparatext.Replace("\n\n", "\n");
+        }
+        newContent = newContent.Replace(oparatext, hparatext);
+
+        if (newContent != member.Content)
+        {
+            await member.EditAsync(new WikiPageEditOptions()
+            {
+                Summary = "Automated cleanup of game page layout.",
+                Bot = true,
+                Minor = true,
+                Watch = AutoWatchBehavior.None,
+                Content = newContent,
+            });
+            Console.WriteLine($"{member.Title} has been updated.");
+        }
+
+        return true;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using dotenv.net;
+using System.Text.RegularExpressions;
+using WikiClientLibrary;
+using WikiClientLibrary.Client;
+using WikiClientLibrary.Generators;
+using WikiClientLibrary.Pages;
+using WikiClientLibrary.Pages.Queries.Properties;
+using WikiClientLibrary.Sites;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DotEnv.Load(new DotEnvOptions(probeForEnv: true, probeLevelsToSearch: 6));
+        MainAsync().Wait();
+    }
+
+    static async Task MainAsync()
+    {
+        using var client = new WikiClient
+        {
+            ClientUserAgent = "APWikiBot/1.0 (Silasary)"
+        };
+        var site = new WikiSite(client, "https://archipelago.miraheze.org/w/api.php");
+        await site.Initialization;
+
+        try
+        {
+            await site.LoginAsync(Environment.GetEnvironmentVariable("botuser")!, Environment.GetEnvironmentVariable("botpass")!);
+        }
+        catch (WikiClientException ex)
+        {
+            Console.WriteLine(ex.Message);
+            return;
+        }
+
+        Console.WriteLine(site.SiteInfo.SiteName);
+        Console.WriteLine($"Connected as {site.AccountInfo}");
+        
+        var myUserPage = new WikiPage(site, "User:" + site.AccountInfo.Name);
+        await myUserPage.RefreshAsync(PageQueryOptions.FetchContent);
+        var env = GetEnv(myUserPage);
+        if (!bool.TryParse(env["Active"], out var active) || !active)
+        {
+            Console.WriteLine("Disabled by UserPage");
+            return;
+        }
+        _ = bool.TryParse(env["RearrangeTemplates"], out bool RearrangeTemplates);
+
+        //var page = new WikiPage(site, "User:Silasary/sandbox");
+        //await GamePageChecks.CheckTemplates(page);
+
+        var games = new WikiPage(site, "Category:Games");
+        await games.RefreshAsync(PageQueryOptions.FetchContent | PageQueryOptions.ResolveRedirects);
+        var catInfo = games.GetPropertyGroup<CategoryInfoPropertyGroup>();
+        Console.WriteLine("Category '{0}' has {1} members.", games.Title, catInfo.MembersCount);
+        var members = new CategoryMembersGenerator(site, "Category:Games");
+        await foreach (var member in members.EnumPagesAsync(PageQueryOptions.FetchContent | PageQueryOptions.ResolveRedirects))
+        {
+            Console.WriteLine(" - {0}", member.Title);
+            if (RearrangeTemplates)
+                await GamePageChecks.CheckTemplates(member);
+        }
+
+        // We're done here
+        await site.LogoutAsync();
+    }
+
+    private static Dictionary<string, string> GetEnv(WikiPage myUserPage)
+    {
+        Regex tableEntry = new("^\\| ([A-Za-z]+) \\|\\| ([A-Za-z]+)", RegexOptions.Multiline);
+        var matches = tableEntry.Matches(myUserPage.Content);
+        var env = new Dictionary<string, string>();
+        foreach (Match match in matches)
+        {
+            env[match.Groups[1].Value] = match.Groups[2].Value;
+            Console.WriteLine($"> {match.Groups[1].Value} = {match.Groups[2].Value}");
+        }
+        return env;
+    }
+}
